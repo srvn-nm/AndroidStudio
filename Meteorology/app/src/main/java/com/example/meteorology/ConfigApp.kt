bb0715b29879ced0d63a0aa6960d194cc69aaf30
Sarvin Nami <srvn0nm@gmail.com>
2022-11-07 14:27:37 +0330

    changing the font style

--- Meteorology/app/src/main/java/com/example/meteorology/ConfigApp.kt
+++ Meteorology/app/src/main/java/com/example/meteorology/ConfigApp.kt
@@ -1,6 +1,25 @@
 package com.example.meteorology
 
+import android.R
 import android.app.Application
+import io.github.inflationx.calligraphy3.CalligraphyConfig
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor
+import io.github.inflationx.viewpump.ViewPump
+
 
 class ConfigApp(): Application() {
+    override fun onCreate() {
+        super.onCreate()
+        ViewPump.init(
+            ViewPump.builder()
+                .addInterceptor(
+                    CalligraphyInterceptor(
+                        CalligraphyConfig.Builder()
+                            .setDefaultFontPath("fonts/vazirmatnmedium.ttf")
+                            .build()
+                    )
+                )
+                .build()
+        )
+    }
 }
