c4927b85b43b84ae1a792365d513f423debc6474
Sarvin Nami <srvn0nm@gmail.com>
2022-11-13 12:10:31 +0330

    using data binding

--- ToDoList/app/src/main/java/com/example/todolist/MenuActivity.kt
+++ ToDoList/app/src/main/java/com/example/todolist/MenuActivity.kt
@@ -2,10 +2,13 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todolist.databinding.ActivityMenuBinding
 
 class MenuActivity : AppCompatActivity() {
+    lateinit var binding : ActivityMenuBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
