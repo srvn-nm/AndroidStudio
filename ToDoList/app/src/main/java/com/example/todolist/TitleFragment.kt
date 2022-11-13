70c012a95ce1789e34c0e2006598e9d3712fe77c
Sarvin Nami <srvn0nm@gmail.com>
2022-11-13 20:01:18 +0330

    adding viewbinding to the fragment

--- ToDoList/app/src/main/java/com/example/todolist/TitleFragment.kt
+++ ToDoList/app/src/main/java/com/example/todolist/TitleFragment.kt
@@ -5,11 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.todolist.databinding.FragmentTitleBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+lateinit var binding: FragmentTitleBinding
 
 /**
  * A simple [Fragment] subclass.
@@ -21,20 +23,11 @@ class TitleFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_title, container, false)
+        binding = FragmentTitleBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     companion object {
