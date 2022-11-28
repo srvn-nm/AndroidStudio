167c4e9b634655f2e24443b4731d9a6bb07ead65
Sarvin Nami <srvn0nm@gmail.com>
2022-11-28 14:41:37 +0330

    adding intent getting extra info

--- ToDoList/app/src/main/java/com/example/todolist/ToDoListActivity.kt
+++ ToDoList/app/src/main/java/com/example/todolist/ToDoListActivity.kt
@@ -5,10 +5,21 @@ import android.os.Bundle
 import com.example.todolist.databinding.ActivityToDoListBinding
 
 class ToDoListActivity : AppCompatActivity() {
-    lateinit var binding : ActivityToDoListBinding
+    private lateinit var binding : ActivityToDoListBinding
+    private lateinit var userIdList:ArrayList<Int>
+    private lateinit var taskIdList:ArrayList<Int>
+    private lateinit var taskTextList:ArrayList<String>
+    private lateinit var taskStateList:ArrayList<String>
+    private lateinit var userInfo:String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding =  ActivityToDoListBinding.inflate(layoutInflater)
+        userIdList = intent.getIntegerArrayListExtra("userIdList") as ArrayList<Int>
+        taskIdList = intent.getIntegerArrayListExtra("taskIdList") as ArrayList<Int>
+        taskTextList = intent.getStringArrayListExtra("taskTextList") as ArrayList<String>
+        taskStateList = intent.getStringArrayListExtra("taskStateList") as ArrayList<String>
+        userInfo = intent.getStringExtra("userInfo") as String
         setContentView(binding.root)
     }
 }
