9f4529b4690ff7a40adf4030941e25bf79fd69be
Sarvin Nami <srvn0nm@gmail.com>
2022-11-28 11:54:02 +0330

    adding binding to the new activity

--- ToDoList/app/src/main/java/com/example/todolist/ToDoListActivity.kt
+++ ToDoList/app/src/main/java/com/example/todolist/ToDoListActivity.kt
@@ -2,10 +2,13 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todolist.databinding.ActivityToDoListBinding
 
 class ToDoListActivity : AppCompatActivity() {
+    lateinit var binding : ActivityToDoListBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_to_do_list)
+        binding =  ActivityToDoListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
