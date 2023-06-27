18bfb61366f99bbc29c1ada919adf18e40285e72
Sarvin Nami <srvn0nm@gmail.com>
2023-06-27 14:20:53 +0330

    using compose in fragment without wml

--- JetPackComposeLearning/app/src/main/java/com/example/jetpackcomposelearning/HomeFragment.kt
+++ JetPackComposeLearning/app/src/main/java/com/example/jetpackcomposelearning/HomeFragment.kt
@@ -1,18 +1,33 @@
 package com.example.jetpackcomposelearning
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
 
 class HomeFragment : Fragment(R.layout.home_fragment) {
 
 
-    //using set content of compose
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+//    //using set content of compose in the xml
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//        view.findViewById<ComposeView>(R.id.compose_view).setContent {
+//            MainContent()
+//        }
+//    }
 
-        view.findViewById<ComposeView>(R.id.compose_view).setContent {
-            MainContent()
+    //without xml file
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                MainContent()
+            }
         }
     }
 }
