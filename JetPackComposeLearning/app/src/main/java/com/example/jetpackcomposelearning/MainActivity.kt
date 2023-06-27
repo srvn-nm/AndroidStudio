575194f062a6e43842bd560196a8e32a398fda6b
Sarvin Nami <srvn0nm@gmail.com>
2023-06-27 11:31:32 +0330

    adding image, using nested functions, adding features to image and cleaning the code

--- JetPackComposeLearning/app/src/main/java/com/example/jetpackcomposelearning/MainActivity.kt
+++ JetPackComposeLearning/app/src/main/java/com/example/jetpackcomposelearning/MainActivity.kt
@@ -4,20 +4,34 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jetpackcomposelearning.ui.theme.JetPackComposeLearningTheme
 
@@ -28,7 +42,7 @@ class MainActivity : ComponentActivity() {
             JetPackComposeLearningTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+//                    modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
                     Greeting("Kitten")
@@ -45,7 +59,7 @@ fun Greeting(name: String) {
 //    Surface(modifier = Modifier.fillMaxWidth().fillMaxHeight()) {
     Text(
         text = "Hello $name ^.^",
-        fontSize = 32.sp,
+        fontSize = 20.sp,
         color = colorResource(id = R.color.purple_700),
         fontFamily = FontFamily.Cursive,
         modifier = Modifier.clickable {
@@ -60,7 +74,34 @@ fun Greeting(name: String) {
 
 @Composable
 fun UserCard() {
-    Row() {
+    val context = LocalContext.current
+    //horizontally
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .padding(12.dp)
+            .border(width = 1.dp, color = Color.Gray)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.download),
+            contentDescription = "",
+            modifier = Modifier
+                .size(120.dp)
+                .clip(
+                    CircleShape
+                ),
+            contentScale = ContentScale.Crop
+        )
+    }
+    //vertically
+    Column {
+        Text(text = stringResource(id = R.string.app_name))
+        Button(onClick = {
+            //action
+        }) {
+            Greeting(name = "Kitten")
+        }
 
     }
 }
@@ -69,6 +110,7 @@ fun UserCard() {
 @Composable
 fun DefaultPreview() {
     JetPackComposeLearningTheme {
-        Greeting("Kitten")
+        //Greeting("Kitten")
+        UserCard()
     }
 }
