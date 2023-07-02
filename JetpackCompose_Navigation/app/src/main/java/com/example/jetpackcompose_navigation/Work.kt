fbfdbb08fb06a677f827c2945c9a27e8886fa687
Sarvin Nami <srvn0nm@gmail.com>
2023-07-02 12:45:27 +0330

    creating work compose

--- JetpackCompose_Navigation/app/src/main/java/com/example/jetpackcompose_navigation/Work.kt
+++ JetpackCompose_Navigation/app/src/main/java/com/example/jetpackcompose_navigation/Work.kt
@@ -3,44 +3,77 @@ package com.example.jetpackcompose_navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetpackcompose_navigation.ui.theme.JetpackCompose_NavigationTheme
+import androidx.compose.ui.unit.dp
 
 class Work : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackCompose_NavigationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
                     Greeting3("Android")
+            }
+        }
+    }
+
+@Composable
+fun Greeting3(name: String) {
+    Card(
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
+        modifier = Modifier
+            .padding(12.dp)
+            .fillMaxWidth()
+            .wrapContentWidth()
+    ) {
+        //horizontally
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentWidth()
+                .padding(12.dp)
+                .border(hight = 1.dp, color = Color.Gray)
+                .padding(12.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.download),
+                contentDescription = "",
+                modifier = Modifier
+                    .size(120.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
+            )
+            //vertically
+            Column {
+                Greeting(name = name)
+                Button(onClick = {
+                    //action
+                }) {
+                    Text(text = "Edit Item")
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting3(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview3() {
-    JetpackCompose_NavigationTheme {
         Greeting3("Android")
-    }
 }
