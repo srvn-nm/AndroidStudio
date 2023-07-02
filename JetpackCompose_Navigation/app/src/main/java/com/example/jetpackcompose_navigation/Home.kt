734a7eccb85bd4227b31d3265a17501efb0fa727
Sarvin Nami <srvn0nm@gmail.com>
2023-07-02 10:27:14 +0330

    adding outlinedTextField for getting inputs

--- JetpackCompose_Navigation/app/src/main/java/com/example/jetpackcompose_navigation/Home.kt
+++ JetpackCompose_Navigation/app/src/main/java/com/example/jetpackcompose_navigation/Home.kt
@@ -3,44 +3,56 @@ package com.example.jetpackcompose_navigation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetpackcompose_navigation.ui.theme.JetpackCompose_NavigationTheme
+import androidx.compose.ui.unit.dp
 
 class Home : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            JetpackCompose_NavigationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting2("Android")
-                }
+                    Greeting2()
             }
         }
     }
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Greeting2(modifier: Modifier = Modifier) {
+    var textInput by remember {
+        mutableStateOf("")
+    }
+    Column(
+        modifier = Modifier.padding(8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        OutlinedTextField(
+            value = textInput,
+            onValueChange ={
+                textInput = it
+            },
+            label = {
+                Text("Number of Works")
+            })
+        for ()
+    }
 }
 
-@Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview2() {
-    JetpackCompose_NavigationTheme {
-        Greeting2("Android")
-    }
+        Greeting2()
 }
