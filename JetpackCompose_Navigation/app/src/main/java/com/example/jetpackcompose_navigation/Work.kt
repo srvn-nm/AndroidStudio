63a376a31b0938a52025a1ad2e6e8f944d05def1
Sarvin Nami <srvn0nm@gmail.com>
2023-07-02 12:25:18 +0330

    debugging

--- JetpackCompose_Navigation/app/src/main/java/com/example/jetpackcompose_navigation/Work.kt
+++ JetpackCompose_Navigation/app/src/main/java/com/example/jetpackcompose_navigation/Work.kt
@@ -12,7 +12,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jetpackcompose_navigation.ui.theme.JetpackCompose_NavigationTheme
 
-class work : ComponentActivity() {
+class Work : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
