86025a65623a1d8b9aa46c86144281a7909316b5
Sarvin Nami <srvn0nm@gmail.com>
2023-07-03 09:54:18 +0330

    adding a new file for navigation

---
+++ compose_navigation/app/src/main/java/com/example/compose_navigation/MainFile.kt
@@ -0,0 +1,9 @@
+package com.example.compose_navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun Navigation(){
+    val navController = rememberNavController(navigators = )
+}
