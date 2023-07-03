33c5ca5d876f3487c4f7e3e1c3c22acfefafa65a
Sarvin Nami <srvn0nm@gmail.com>
2023-07-03 09:32:12 +0330

    2 fragments

--- JetpackCompose_Navigation/app/src/main/java/com/example/jetpackcompose_navigation/BlankFragment.kt
+++ JetpackCompose_Navigation/app/src/main/java/com/example/jetpackcompose_navigation/BlankFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [firstFragment.newInstance] factory method to
+ * Use the [BlankFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class firstFragment : Fragment() {
+class BlankFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -31,14 +31,14 @@ class firstFragment : Fragment() {
     }
 
     @Deprecated("Deprecated in Java",
-        ReplaceWith("inflater.inflate(R.layout.fragment_first, container, false)")
+        ReplaceWith("inflater.inflate(R.layout.fragment_blank, container, false)")
     )
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_blank, container, false)
     }
 
     companion object {
@@ -48,12 +48,12 @@ class firstFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment firstFragment.
+         * @return A new instance of fragment BlankFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            firstFragment().apply {
+            BlankFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
