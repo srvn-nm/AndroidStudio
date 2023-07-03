c0e29e8edf15a83126cf5e0ce4da7d2531638d6e
Sarvin Nami <srvn0nm@gmail.com>
2023-07-03 12:07:34 +0330

    adding RetrofitInstance and declaring retrofit builder and create

--- RetrofitLearning/app/src/main/java/com/example/retrofitlearning/api/RetrofitInstance.kt
+++ RetrofitLearning/app/src/main/java/com/example/retrofitlearning/api/RetrofitInstance.kt
@@ -1,4 +1,17 @@
 package com.example.retrofitlearning.api
 
+import com.example.retrofitlearning.utils.Contants.Companion.BASE_URL
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitInstance {
+    private val retrofit by lazy{
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+    val api:SimpleApi by lazy {
+        retrofit.create(SimpleApi::class.java)
+    }
 }
