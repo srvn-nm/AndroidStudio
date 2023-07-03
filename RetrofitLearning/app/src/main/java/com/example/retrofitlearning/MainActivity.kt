adc33e342c7c7c4aa7f7462a5283f60cd90a4b15
Sarvin Nami <srvn0nm@gmail.com>
2023-07-03 12:58:37 +0330

    showing the results

--- RetrofitLearning/app/src/main/java/com/example/retrofitlearning/MainActivity.kt
+++ RetrofitLearning/app/src/main/java/com/example/retrofitlearning/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.retrofitlearning
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -9,12 +10,40 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.retrofitlearning.repository.Repository
 import com.example.retrofitlearning.ui.theme.RetrofitLearningTheme
+import kotlin.properties.Delegates
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var  viewModel: MainViewModel
+    var id:Int = 0
+    lateinit  var title:String
+    lateinit var body:String
+    var userId :Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val repository = Repository()
+        val viewModelFactory = MainViewModelFactory(repository)
+        viewModel = ViewModelProvider(this, viewModelFactory).get(MainViewModel::class.java)
+        viewModel.getPost()
+        viewModel.myResponse.observe(this, Observer {response ->
+            Log.d("Response userId", response.userId.toString())
+            Log.d("Response id", response.id.toString())
+            Log.d("Response title", response.title)
+            Log.d("Response body", response.body)
+            userId = response.userId
+            id = response.id
+            title = response.title
+            body = response.body
+
+
+        })
+
         setContent {
             RetrofitLearningTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,25 +51,26 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting(userId = userId, id = id, title = title, body = body)
                 }
             }
         }
+
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(userId:Int, id:Int, title:String, body:String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "UserId: $userId\nId: $id\nTitle: $title\nBody: $body",
         modifier = modifier
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RetrofitLearningTheme {
-        Greeting("Android")
-    }
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    RetrofitLearningTheme {
+//        Greeting(userId = response.userId, id = response.id, title = response.title, body = response.body)
+//    }
+//}
