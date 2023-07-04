482e4b1aafae982f1d2ba365276a4f7a3d9c6d7a
Sarvin Nami <srvn0nm@gmail.com>
2023-07-04 12:32:36 +0330

    completing the download of default image

--- GlidePractice/app/src/main/java/com/example/glidepractice/utils/ImageUtils.kt
+++ GlidePractice/app/src/main/java/com/example/glidepractice/utils/ImageUtils.kt
@@ -6,22 +6,30 @@ import androidx.annotation.DrawableRes
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.target.CustomTarget
 import com.bumptech.glide.request.transition.Transition
 
 @Composable
-fun loadPictures(url: String, @DrawableRes defaltImage:Int): MutableState<Bitmap?>{
-    val bitmapState :MutableState<Bitmap?> = mutableStateOf(null)
-    Glide.with(LocalContext.current).asBitmap().load(defaltImage).into(object: CustomTarget<Bitmap>(){
-        override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+fun loadPictures(url: String, @DrawableRes defaltImage: Int): MutableState<Bitmap?> {
+    val bitmapState: MutableState<Bitmap?> = remember{mutableStateOf(null)}
 
-        }
+    //Downloading the default image
+    Glide.with(LocalContext.current).asBitmap().load(defaltImage)
+        .into(object : CustomTarget<Bitmap>() {
+            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                bitmapState.value = resource
+            }
 
-        override fun onLoadCleared(placeholder: Drawable?) {
+            override fun onLoadCleared(placeholder: Drawable?) {
 
-        }
+            }
+        })
 
-    })
+
+    //Downloading the real image
+
+    return bitmapState
 }
